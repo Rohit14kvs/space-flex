08f4e6c72c90d593afaefd36dd4f97cc28413fde
Rohit Singh <rohit14kvs@gmail.com>
2021-09-17 15:59:02 +0530

    Cards redesigned

--- app.js
+++ app.js
@@ -3,20 +3,18 @@ const auth = "MFdCH7ZORiRbhucD5T1sleJYWrtGbKbg6J9BnBIV";
 const imageCard = document.querySelector(".picture-card");
 const moreBtn = document.querySelector(".more-btn");
 
-
-moreBtn.addEventListener('click', fetchAPI);
+moreBtn.addEventListener("click", fetchAPI);
 
 async function fetchAPI() {
-    const link = `https://api.nasa.gov/planetary/apod?api_key=${auth}&count=10`;
-    const response = await fetch(link);
-    const data = await response.json();
-    console.log(data);
-    addPicture(data);
+  const link = `https://api.nasa.gov/planetary/apod?api_key=${auth}&count=12`;
+  const response = await fetch(link);
+  const data = await response.json();
+  console.log(data);
+  addPicture(data);
 }
 
 function addPicture(data) {
   for (let i = 0; i < data.length; i++) {
-
     const Img = document.createElement("div");
     Img.classList.add("img-sec");
     Img.innerHTML = `<img src = ${data[i].hdurl}></img>
@@ -25,10 +23,24 @@ function addPicture(data) {
                 <div class="img-title">${data[i].title}</div>
                 <div class="img-date">${data[i].date}</div>
                 </div>
+                <div class="img-btns">
+                <div class="heart" id ="hrt"></div>
+                <div class="share"><a id="share-url" href=${data[i].url}>Share <i class="fas fa-location-arrow"></i></a></div>
+                </div>
                 <p class="img-desc">${data[i].explanation}</p>
                 </div>`;
     imageCard.appendChild(Img);
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const heart = document.getElementsByClassName("heart");
+    console.log(heart);
+    heart.forEach(btn => {
+        btn.addEventListener("click", function() {
+            this.classList.toggle("is-active");
+          });
+    });
+});
 
 fetchAPI();
