986435c4cb4c624908cb09581e0a9f62ade44d02
Rohit Singh <rohit14kvs@gmail.com>
2021-09-17 21:27:16 +0530

    Added copy to clip board

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const auth = "MFdCH7ZORiRbhucD5T1sleJYWrtGbKbg6J9BnBIV";
-
 const imageCard = document.querySelector(".picture-card");
 const moreBtn = document.querySelector(".more-btn");
-let url;
+
 moreBtn.addEventListener("click", fetchAPI);
 
 async function fetchAPI() {
   const link = `https://api.nasa.gov/planetary/apod?api_key=${auth}&count=12`;
   const response = await fetch(link);
   const data = await response.json();
-  console.log(data);
+//   console.log(data);
   addPicture(data);
+
   const like = document.querySelectorAll(".fa-heart");
   const like_array = [...like];
 
@@ -20,6 +20,16 @@ async function fetchAPI() {
       console.log(like_array[i]);
     });
   }
+}
+
+function copyToClipboard(href) {
+    const elem = document.createElement('textarea');
+    elem.value = href;
+    document.body.appendChild(elem);
+    elem.select();
+    document.execCommand('copy');
+    document.body.removeChild(elem);
+    alert("Copied To Clipboard");
 }
 
 function addPicture(data) {
@@ -34,7 +44,7 @@ function addPicture(data) {
                 </div>
                 <div class="img-btns">
                 <div class="heart" id="hrt"><i class="fas fa-heart"></i></div>
-                <div class="share"><a id="share-url" href="">Share <i class="fas fa-location-arrow"></i></a></div>
+                <div class="share"><a id="share-url" onclick="copyToClipboard('${data[i].url}')">Share <i class="fas fa-location-arrow"></i></a></div>
                 </div>
                 <p class="img-desc">${data[i].explanation}</p>
                 </div>`;
